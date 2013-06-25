d6ef1b290abb010928ddc6c0315b68d7ecaa5dc3
Matthieu Tanguay-Carel <matt@matthias.(none)>
2013-06-25 15:22:05 +0200

    using test registro and name of worksheet to get to the right sheet

--- app/models/registro.rb
+++ app/models/registro.rb
@@ -1,7 +1,7 @@
 class Registro
   def add_row cell_values
     session = GoogleDrive.login(ENV['gmail'], ENV['gmailp'])
-    ws = session.spreadsheet_by_title("Registro").worksheets[0]
+    ws = session.spreadsheet_by_title("TestRegistro").worksheet_by_title("Registro")
     ws.update_cells(ws.num_rows + 1, 1, [cell_values.map{|v|v.strip}])
     ws.save
   end
